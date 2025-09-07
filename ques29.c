c24caa47ace37823db1b0f9288d4287ea94ccbdb
farizz26 <farizkhan9765@gmail.com>
2025-09-07 19:59:34 +0530

    ques29.c

---
+++ ques29.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+//Write a program to calculate the factorial of a number.
+
+long long factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    long long fact;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        fact = factorial(num);
+        printf("Factorial of %d = %lld\n", num, fact);
+    }
+
+    return 0;
+}
