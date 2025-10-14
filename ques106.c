9e410d296ee099a4f5da510889e590e010667ccd
Fariz khan <farizkhan9765@gmail.com>
2025-10-14 22:40:53 +0530

    ques106.c

---
+++ ques106.c
@@ -0,0 +1,48 @@
+Write a program to take an integer array nums of size n, and print the majority element. The majority element is the element that appears strictly more than ⌊n / 2⌋ times. Print -1 if no such element exists. Note: Majority Element is not necessarily the element that is present most number of times.
+
+  #include <stdio.h>
+
+int findMajorityElement(int nums[], int n) {
+    int count = 0, candidate = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (count == 0) {
+            candidate = nums[i];
+            count = 1;
+        } else if (nums[i] == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    count = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == candidate) {
+            count++;
+        }
+    }
+
+    if (count > n / 2) {
+        return candidate;
+    } else {
+        return -1;
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int result = findMajorityElement(nums, n);
+    printf("Majority Element: %d\n", result);
+
+    return 0;
+}
