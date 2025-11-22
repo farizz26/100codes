e35476f3b65399893d1e1df99c090492de518db0
Fariz khan <farizkhan9765@gmail.com>
2025-11-22 20:00:24 +0530

    Define Person structure with members
    Added a structure definition for 'Person' with name, age, and height.

---
+++ ques143.c
@@ -0,0 +1,14 @@
+//Write a function that accepts a structure as parameter and prints its members.
+
+
+
+
+
+
+#include <stdio.h>
+
+struct Person {
+    char name[50];
+    int age;
+    float height;
+};
