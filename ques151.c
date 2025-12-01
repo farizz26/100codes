4a3b42b1da695bce2d1b279b305486f57bbe53ee
Fariz khan <farizkhan9765@gmail.com>
2025-12-01 09:04:29 +0530

    Implement student data modification using pointers
    This program uses a pointer to a struct to modify and display student data.

---
+++ ques151.c
@@ -0,0 +1,32 @@
+//Q150: Use pointer to struct to modify and display data using -> operator.
+
+/*
+Sample Test Cases:
+Input 1:
+Student pointer modifying values: John 106 91
+Output 1:
+Modified Data: Name: John | Roll: 106 | Marks: 91
+
+*/
+
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student s;           // structure variable
+    struct Student *ptr = &s;   // pointer to struct
+
+    // Input format: name roll marks
+    scanf("%s %d %d", ptr->name, &ptr->roll, &ptr->marks);
+
+    // Display modified data
+    printf("Modified Data: Name: %s | Roll: %d | Marks: %d",
+           ptr->name, ptr->roll, ptr->marks);
+
+    return 0;
+}
